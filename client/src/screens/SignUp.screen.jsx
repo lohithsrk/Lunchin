f7658e75fc96296dcd8b5c7d043b963a16964a1d
LegalPirates2020 <legalpirates2020@gmail.com>
2021-09-28 15:51:03 +0530

    Changes

--- client/src/screens/SignUp.screen.jsx
+++ client/src/screens/SignUp.screen.jsx
@@ -1,52 +1,86 @@
 import React from 'react';
-import { View, Text, StatusBar, Image, TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+	View,
+	Text,
+	StatusBar,
+	Image,
+	TouchableOpacity,
+	SafeAreaView,
+	ScrollView,
+	TextInput
+} from 'react-native';
 
 export default function SignUp() {
 	return (
 		<SafeAreaView style={{ marginTop: StatusBar.currentHeight }}>
-			<View
-				style={{
-					width: '100%',
-					alignItems: 'center',
-					justifyContent: 'center',
-					marginTop: 10
-				}}
-			>
-				<View style={{ alignItems: 'center',width:'100%' }}>
-					<Text style={{ fontSize: 40, fontWeight: '700' }}>Sign Up</Text>
-					<View
-						style={{
-							backgroundColor: '#5D5FEF',
-							width: 90,
-							height: 3,
-						}}
-					></View>
-				</View>
-			</View>
-			<View>
-				<Image source={require('../../assets/Signup/avatar.png')} />
-				<TouchableOpacity
-					activeOpacity={0.5}
+			<ScrollView>
+				<View
 					style={{
-						backgroundColor: '#FFE8B2',
+						width: '100%',
 						alignItems: 'center',
 						justifyContent: 'center',
-						paddingVertical: 10,
-						paddingHorizontal: 20,
-						borderRadius: 30,
-						marginTop: 30,
-						width: '40%',
-						position: 'absolute',
-						top: 60,
-						right: 30,
-						zIndex: 3
+						marginBottom: 30
 					}}
-					onPress={() => navigation.navigate('Home')}
 				>
-					<Text style={{ fontSize: 22, fontWeight: '700' }}>Sign Up</Text>
-				</TouchableOpacity>
-			</View>
+					<View style={{ alignItems: 'center', width: '100%' }}>
+						<Text style={{ fontSize: 40, fontWeight: '700' }}>Sign Up</Text>
+						<View
+							style={{
+								backgroundColor: '#5D5FEF',
+								width: 90,
+								height: 3
+							}}
+						></View>
+					</View>
+				</View>
+				<View
+					style={{
+						alignItems: 'center',
+						justifyContent: 'center',
+						backgroundColor: '#5D5FEF',
+						paddingVertical: 30
+					}}
+				>
+					<Image
+						source={require('../../assets/Signup/avatar.png')}
+						style={{ width: 150, height: 150 }}
+					/>
+					<TouchableOpacity
+						activeOpacity={0.5}
+						style={{
+							backgroundColor: '#FFE8B2',
+							alignItems: 'center',
+							justifyContent: 'center',
+							paddingVertical: 10,
+							paddingHorizontal: 30,
+							borderRadius: 30,
+							marginTop: 15
+						}}
+						onPress={() => navigation.navigate('Home')}
+					>
+						<Text style={{ fontSize: 22, fontWeight: '700' }}>
+							Upload Picture
+						</Text>
+					</TouchableOpacity>
+				</View>
+				<View>
+					<View>
+						<TextInput placeholder='Name' />
+					</View>
+					<View>
+						<TextInput placeholder='Email' />
+					</View>
+					<View>
+						<TextInput placeholder='Mobile number' />
+					</View>
+					<View>
+						<TextInput placeholder='Alternate mobile number' />
+					</View>
+					<View>
+						<TextInput placeholder='FSSAI Certificate' />
+					</View>
+				</View>
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
