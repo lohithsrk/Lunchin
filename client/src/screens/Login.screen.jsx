479c3a23d3461fa1bb0b3ec711df9def3fe77af1
solomonsanthosh <solosanty2064@gmail.com>
2021-09-29 16:47:03 +0530

    over

--- client/src/screens/Login.screen.jsx
+++ client/src/screens/Login.screen.jsx
@@ -1,28 +1,44 @@
 import React from "react";
-import { View, Text, SafeAreaView, StatusBar } from "react-native";
+import { View, Text, SafeAreaView, StatusBar ,TextInput } from "react-native";
 
 export default function Login() {
   return (
     <SafeAreaView style={{ marginTop: StatusBar.currentHeight }}>
       <View
+        style={{
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
         <Text
           style={{
             fontWeight: "700",
             fontSize: 40,
-            textAlign: 'center'
           }}
         >
           Log In
         </Text>
-        
-      </View> 
+        <View
+          style={{
+            marginTop: 5,
+            backgroundColor: "#FF0A0A",
+            width: 80,
+            height: 5,
+          }}
+        ></View>
+      </View>
       <View
         style={{
-          position: "absolute",
+          justifyContent:'center'
+          
         }}
       >
-       
+        <TextInput
+          placeholder="homechef@gmail.com"
+          
+        />
+        
       </View>
     </SafeAreaView>
   );
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,31 +2,31 @@ const User = require('../modals/user');
 const cloudinary = require('cloudinary')
 
 
-cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET
-})
+// cloudinary.config({
+// 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+// 	api_key: process.env.CLOUDINARY_API_KEY,
+// 	api_secret: process.env.CLOUDINARY_API_SECRET
+// })
 
 exports.createUser = async (req, res) => {
-	const { email, username, phone, password,certificate,photo } = req.body;
+	const { email, username, phone, password } = req.body;
 	console.log(req.body);
-	let image = await cloudinary.uploader.upload(certificate, {
-		public_id: `${Date.now( )}`,
-		resource_type: 'auto'
-	});
-	res.json({
-		public_id: image.public_id,
-		url: image.secure_url
-	})
+	// let image = await cloudinary.uploader.upload(certificate, {
+	// 	public_id: `${Date.now( )}`,
+	// 	resource_type: 'auto'
+	// });
+	// res.json({
+	// 	public_id: image.public_id,
+	// 	url: image.secure_url
+	// })
 	const user = new User({
 		username,
 		email,
-		phone,
-		certificate : {
-			public_id: image.public_id,
-			url: image.secure_url
-		}
+		phone
+		// certificate : {
+		// 	public_id: image.public_id,
+		// 	url: image.secure_url
+		// }
 	});
 	
 	const newUser = await User.register(user, password);
