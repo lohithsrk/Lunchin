966455d14ab309df7de43d12d2cf948bb2d8784a
LegalPirates2020 <legalpirates2020@gmail.com>
2021-09-30 09:51:32 +0530

    Changes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,34 +1,33 @@
 const User = require('../modals/user');
-const cloudinary = require('cloudinary')
-
+const cloudinary = require('cloudinary');
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET
-})
+});
 
 exports.createUser = async (req, res) => {
-	const { email, username, phone, password,certificate,photo } = req.body;
+	const { email, username, phone, password, certificate, photo } = req.body;
 	console.log(req.body);
 	let image = await cloudinary.uploader.upload(certificate, {
-		public_id: `${Date.now( )}`,
+		public_id: `${Date.now()}`,
 		resource_type: 'auto'
 	});
 	res.json({
 		public_id: image.public_id,
 		url: image.secure_url
-	})
+	});
 	const user = new User({
 		username,
 		email,
 		phone,
-		certificate : {
+		certificate: {
 			public_id: image.public_id,
 			url: image.secure_url
 		}
 	});
-	
+
 	const newUser = await User.register(user, password);
 	console.log(newUser);
 	res.json(newUser);
